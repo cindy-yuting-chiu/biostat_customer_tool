6f0d3d517795695c1800a4aceda50a5a39ff70fd
Cindy Chiu <yc506@duke.edu>
2022-04-20 22:00:19 -0400

    add testing code

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.types import DateTime
 from sqlalchemy.orm import relationship
-# import the connection to this file 
-from .database import Base
+
+# import the connection to this file
+from database import Base
 
 # Class for doctors table
 class Doctors(Base):
@@ -12,11 +13,12 @@ class Doctors(Base):
     DoctorName = Column(String(255))
     Speciality = Column(String(255))
     AvailableTime = Column(String(1225))
-    # relationship between these two tables 
+    # relationship between these two tables
     # can be called using Doctors.appointment
     appointment = relationship("Appointments", back_populates="doctor")
 
-# Class for appointments table 
+
+# Class for appointments table
 class Appointments(Base):
     __tablename__ = "appointments"
 
@@ -24,6 +26,6 @@ class Appointments(Base):
     DoctorID = Column(Integer, ForeignKey("doctors.DoctorID"))
     AppointmentTime = Column(DateTime)
     PatientID = Column(String(225))
-    # relationship between these two tables 
+    # relationship between these two tables
     # can be called using Appointments.doctor
     doctor = relationship("Doctors", back_populates="appointment")
